e8ecc2dffda7b21f77ba7b5798954e05a8a7ef90
Matīss Komļevs <matisskomlevs@gmail.com>
2022-02-18 18:18:12 +0200

    added country capital
    basic understanding of working with GraphQL queries

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const selectContinent = document.getElementById('selectContinent') //select tag
 const countriesList = document.getElementById('countriesList')
+const countryProps = document.getElementById('countryProps')
 
 queryFetch(`
   query {
@@ -19,26 +20,32 @@ queryFetch(`
 
 selectContinent.addEventListener('change', async (e) => {
   const continentCode = e.target.value
-  const countries =  await getContinentCountries(continentCode)
+  const countries =  await getCountriesFromContinent(continentCode)
   countriesList.innerHTML = ''
   countries.forEach(country => {
     const div = document.createElement('div')
-    div.innerText = country.name
+    div.innerHTML = `
+      <h2>Country: ${country.name}</h2>
+      <h2>Capital: ${country.capital}</h2>
+      <br>
+    `
     countriesList.append(div)
   })
 })
 
-function getContinentCountries (continentCode) {
+function getCountriesFromContinent (continentCode) {
   return queryFetch(`
-    query getThemAll ($code:ID!) {
+    query getThemCountries ($code:ID!) {
       continent(code: $code) {
         countries{
           name
+          capital
         }
       }
     }
   `, {code: continentCode}).then(data => {
-    return data.data.continent.countries
+    let allCountries = data.data.continent.countries
+    return allCountries
   })
 }
 
